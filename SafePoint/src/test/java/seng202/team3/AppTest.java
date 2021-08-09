d319b86716935d447b74e2a4ef0c47da4caa6ad8
Matthew <mga114@uclive.ac.nz>
2021-08-09 13:29:48 +1200

    removed tests

--- SafePoint/src/test/java/seng202/team3/AppTest.java
+++ SafePoint/src/test/java/seng202/team3/AppTest.java
@@ -39,10 +39,10 @@ public class AppTest
     /**
      * Priscilla's test for checklist
      */
-    public void testAppPriscilla() { assertTrue(true); }
+    //public void testAppPriscilla() { assertTrue(true); }
 
     /**
      * Danish's test for checklist
      */
-    public void testAppDanish() { assertTrue(true);}
+    //public void testAppDanish() { assertTrue(true);}
 }
