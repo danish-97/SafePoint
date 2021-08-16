0259c692a1a29531770a0b2844bfe0a0da27d0b4
Rory <rho66@uclive.ac.nz>
2021-08-17 10:33:48 +1200

    Created DataManager branch

---
+++ src/main/java/seng202/team3/model/DataManager.java
@@ -0,0 +1,17 @@
+package seng202.team3.model;
+
+
+public class Datamanager {
+
+    private String[] acitveDataLocation;
+    private allCrimeData ArrayList<CrimeData> = new ArrayList<CrimeData>;
+    private activeCrimeData ArrayList<CrimeData> = new ArrayList<CrimeData>;
+
+    public ArrayList<CrimeData> getData() {
+        //TODO IMPLEMENT getData()
+    }
+
+    public CrimeData getDataByID(string) {
+        //TODO IMPLEMENT getDataByID
+    }
+}
