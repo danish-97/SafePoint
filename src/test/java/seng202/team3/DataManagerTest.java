b02677d92ab5e11bd852a7073984b45e2f2d5c5e
Rory Holmes <rho66@uclive.ac.nz>
2021-08-17 13:19:04 +1200

    Added test case for getDataByID

--- src/test/java/seng202/team3/DataManagerTest.java
+++ src/test/java/seng202/team3/DataManagerTest.java
@@ -1,4 +1,16 @@
 package test.java.seng202.team3;
 
+import main.java.seng202.team3.model.DataManager;
+
 public class DataManagerTest {
+
+    /**
+     * Test getDataByID returns correct CrimeData object.
+     */
+    public void getDataByIDTest() {
+        Object data = new DataManager();
+        CrimeData crimeData = new CrimeData("JE123990", "27/08/2021", "1 RANDOM STREET", "THEFT" );
+        CrimeData crimeTest = data.getDataByID("JE123990");
+        assertEquals(crimeData, crimeTest);
+    }
 }
