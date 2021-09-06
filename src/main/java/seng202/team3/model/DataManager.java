843f12b5e070b85c0a8a3709b2776494acd1da59
Rory Holmes <rho66@uclive.ac.nz>
2021-09-06 12:37:13 +1200

    Made getDataByID method

--- src/main/java/seng202/team3/model/DataManager.java
+++ src/main/java/seng202/team3/model/DataManager.java
@@ -1,5 +1,6 @@
 package main.java.seng202.team3.model;
 
+import seng202.team3.model.CrimeStat;
 
 public class DataManager {
 
@@ -7,13 +8,23 @@ public class DataManager {
     private ArrayList<CrimeData> allCrimeData = new ArrayList<CrimeData>();
     private ArrayList<CrimeData> activeCrimeDataactiveCrimeData = new ArrayList<CrimeData>();
 
-    public ArrayList<CrimeData> getData() {
+   /* public ArrayList<CrimeData> getData() {
         //TODO IMPLEMENT getData()
         return NULL;
     }
+    */
+   public CrimeData getDataByID(String ID) throws Exception {
+        //Loops through allCrimeData and returns Crime with matching ID
+        CrimeData matchingCrime = null;
+        for (CrimeData crime : allCrimeData) {
+            if (crime.getCrimeId() == ID) {
+                matchingCrime = crime;
+            }
+        }
+        if (matchingCrime == null) {
+            throw new Exception("Crime not found excpetion");
 
-    public CrimeData getDataByID(String ID) {
-        //TODO IMPLEMENT getDataByID
-        return NULL;
+        } else return matchingCrime;
     }
+
 }
