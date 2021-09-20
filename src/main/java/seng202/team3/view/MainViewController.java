02b743f6d28b8cf2d49cd61e43bec258555e169f
Priscilla <pis19@uclive.ac.nz>
2021-09-21 02:09:52 +1200

    Edited pom file to include Google Maps API, integrated Google Maps in general to project by adding a new pane to the GUI. Fixed Maven dependency issues.

--- src/main/java/seng202/team3/view/MainViewController.java
+++ src/main/java/seng202/team3/view/MainViewController.java
@@ -1,4 +1,38 @@
 package seng202.team3.view;
 
-public class MainViewController {
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+import javax.swing.text.Position;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
+
+    @FXML
+    private WebView mapView;
+
+    private WebEngine webEngine;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        initMap();
+
+    }
+
+    private void initMap() {
+        webEngine = mapView.getEngine();
+        webEngine.load(getClass().getClassLoader().getResource("seng202.team3.view/googleMap.html").toExternalForm());
+    }
+
 }
+
