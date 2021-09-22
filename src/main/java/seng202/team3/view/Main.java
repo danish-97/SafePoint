8d659208bf31c2ccaf5b1b7e21b4840e737f27f4
mga114 <84060777+mga114@users.noreply.github.com>
2021-09-22 12:11:44 +1200

    GUI interaction framework in MainViewController done for first 3 panels in the GUI

--- src/main/java/seng202/team3/view/Main.java
+++ src/main/java/seng202/team3/view/Main.java
@@ -7,13 +7,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application{
     @Override
     public void start(Stage stage) throws IOException {
 
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("seng202.team3.view/main-view.fxml"));
-        stage.setTitle("Hello!");
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("seng202.team3.view/main-view.fxml")));
+        stage.setTitle("SafePoint");
         stage.setScene(new Scene(root, 1000, 600));
         stage.show();
     }
--- src/main/java/seng202/team3/view/MainViewController.java
+++ src/main/java/seng202/team3/view/MainViewController.java
@@ -2,20 +2,37 @@ package seng202.team3.view;
 
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
-import javax.swing.text.Position;
-import java.net.URL;
-import java.util.ResourceBundle;
-
 public class MainViewController implements Initializable {
+
+    @FXML
+    private TextField regionFilter;
+
+    @FXML
+    private ChoiceBox crimeSelector;
+
+    @FXML
+    private CheckBox policeDataToggle;
+
+    @FXML
+    private CheckBox userDataToggle;
+
+    @FXML
+    private CheckBox arrestMadeToggle;
+
+    @FXML
+    private CheckBox graphToggle;
 
     @FXML
     private WebView mapView;
@@ -26,13 +43,43 @@ public class MainViewController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         initMap();
-
+        initCrimeSelector();
     }
 
     private void initMap() {
         webEngine = mapView.getEngine();
-        webEngine.load(getClass().getClassLoader().getResource("seng202.team3.view/googleMap.html").toExternalForm());
+        webEngine.load(Objects.requireNonNull(getClass().getClassLoader().getResource("seng202.team3.view/googleMap.html")).toExternalForm());
     }
 
+    public void initCrimeSelector () {
+        String[] crimeTypes = {"ARSON", "ASSAULT", "BATTERY", "BURGLARY", "CONCEALED CARRY LICENCE", "CRIMINAL DAMAGE", "CRIMINAL SEXUAL ASSAULT", "CRIMINAL TRESPASS", "DECEPTIVE PRACTICE", "HOMICIDE", "INTERFERENCE WITH PUBLIC OFFICER", "INTIMIDATION", "KIDNAPPING", "LIQUOR LAW VIOLATION", "MOTOR VEHICLE THEFT", "NARCOTICS", "OFFENSE INVOLVING CHILDREN", "OTHER NARCOTIC VIOLATION", "OTHER OFFENSE", "PROSTITUTION", "PUBLIC PEACE VIOLATION", "ROBBERY", "SEX OFFENSE", "STALKING", "THEFT", "WEAPONS VIOLATION"};
+        for (String crimeType : crimeTypes) {
+            crimeSelector.getItems().add(crimeType);
+        }
+    }
+
+    public String getCurrentRegion () {
+        return regionFilter.getText();
+    }
+
+    public String getCurrentCrimeType () {
+        return (String) crimeSelector.getValue();
+    }
+
+    public Boolean getPoliceDataToggle() {
+        return policeDataToggle.isSelected();
+    }
+
+    public Boolean getUserDataToggle() {
+        return userDataToggle.isSelected();
+    }
+
+    public Boolean getArrestMadeToggle() {
+        return arrestMadeToggle.isSelected();
+    }
+
+    public Boolean getGraphToggle() {
+        return graphToggle.isSelected();
+    }
 }
 
