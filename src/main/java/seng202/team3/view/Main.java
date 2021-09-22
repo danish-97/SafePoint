2853c5c170281f3c596c81ce4287834f9b00da94
mga114 <mga114@uclive.ac.nz>
2021-09-22 17:24:49 +1200

    Added new Main class to launch application

--- src/main/java/seng202/team3/view/Main.java
+++ src/main/java/seng202/team3/view/Main.java
@@ -1,25 +1,8 @@
 package seng202.team3.view;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.util.Objects;
-
-public class Main extends Application{
-    @Override
-    public void start(Stage stage) throws IOException {
-
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("seng202.team3.view/main-view.fxml")));
-        stage.setTitle("SafePoint");
-        stage.setScene(new Scene(root, 1000, 600));
-        stage.show();
-    }
+public class Main{
 
     public static void main(String[] args) {
-        launch(args);
+        SafePoint.main(args);
     }
 }
