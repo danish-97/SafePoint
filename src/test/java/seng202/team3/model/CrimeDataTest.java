2223eff166751b4ba0e8dd7185b0ab16b9eaea73
Danish Jahangir <dkj23@uclive.ac.nz>
2021-09-25 13:55:11 +1200

    Added CrimeDataTest to this branch and fixed PoliceDataTest

---
+++ src/test/java/seng202/team3/model/CrimeDataTest.java
@@ -0,0 +1,65 @@
+package seng202.team3.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for CrimeDataTest
+ */
+
+public class CrimeDataTest {
+
+    /**
+     * Tests if the getAddress method works as intended.
+     * @throws ParseException checks if the format is correct when string is parsed.
+     */
+    @Test
+    public void checkAddress() throws ParseException {
+        String[] strSplit = "JE266628,06/15/2021 09:30:00 AM,080XX S DREXEL AVE,0820,THEFT,$500 AND UNDER,STREET,N,N,631,8,06,1183633,1851786,41.748486365,-87.602675062,(41.748486365, -87.602675062)".split(",");
+        ArrayList<String> data = new ArrayList<>(Arrays.asList(strSplit));
+        PoliceData pData = new PoliceData("1", data);
+        assertEquals("080XX S DREXEL AVE", pData.getAddress());
+    }
+
+    /**
+     * Tests if the getData method works as intended.
+     * @throws ParseException checks if the format is correct when string is parsed.
+     */
+    @Test
+    public void checkDate() throws ParseException {
+        String[] strSplit = "JE266628,06/15/2021 09:30:00 AM,080XX S DREXEL AVE,0820,THEFT,$500 AND UNDER,STREET,N,N,631,8,06,1183633,1851786,41.748486365,-87.602675062,(41.748486365, -87.602675062)".split(",");
+        ArrayList<String> data = new ArrayList<>(Arrays.asList(strSplit));
+        PoliceData pData = new PoliceData("1", data);
+        assertEquals("06/15/2021 09:30:00 AM", pData.getDate());
+    }
+
+    /**
+     * Checks if the getLocation method works as intended.
+     * @throws ParseException checks if the format is correct when string is parsed.
+     */
+    @Test
+    public void checkLocation() throws ParseException {
+        String[] strSplit = "JE266628,06/15/2021 09:30:00 AM,080XX S DREXEL AVE,0820,THEFT,$500 AND UNDER,STREET,N,N,631,8,06,1183633,1851786,41.748486365,-87.602675062,(41.748486365, -87.602675062)".split(",");
+        ArrayList<String> data = new ArrayList<>(Arrays.asList(strSplit));
+        PoliceData pData = new PoliceData("1", data);
+        assertEquals("41.748486365, -87.602675062", pData.getLocation());
+    }
+
+    /**
+     * Checks if the getCrimeType method works as intended.
+     * @throws ParseException checks if the format is correct when string is parsed.
+     */
+    @Test
+    public void checkCrimeType() throws ParseException {
+        String[] strSplit = "JE266628,06/15/2021 09:30:00 AM,080XX S DREXEL AVE,0820,THEFT,$500 AND UNDER,STREET,N,N,631,8,06,1183633,1851786,41.748486365,-87.602675062,(41.748486365, -87.602675062)".split(",");
+        ArrayList<String> data = new ArrayList<>(Arrays.asList(strSplit));
+        PoliceData pData = new PoliceData("1", data);
+        assertEquals("THEFT", pData.getCrimeType());
+    }
+
+}
--- src/test/java/seng202/team3/model/PoliceDataTest.java
+++ src/test/java/seng202/team3/model/PoliceDataTest.java
@@ -8,22 +8,27 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * Unit test for PoliceData test
+ */
 
 public class PoliceDataTest {
 
     /**
-     * Checks if the getCaseNumber method gets the correct field from the String.
+     * Checks if the getCaseNumber method gets the correct field from the ArrayList.
+     * @throws ParseException checks if the format is correct when string is parsed.
      */
     @Test
     public void caseNumberCheck() throws ParseException {
         String[] strSplit = "JE266628,06/15/2021 09:30:00 AM,080XX S DREXEL AVE,0820,THEFT,$500 AND UNDER,STREET,N,N,631,8,06,1183633,1851786,41.748486365,-87.602675062,(41.748486365, -87.602675062)".split(",");
         ArrayList<String> data = new ArrayList<>(Arrays.asList(strSplit));
         PoliceData pData = new PoliceData("1", data);
-        assertEquals("JE163990", pData.getCaseNumber());
+        assertEquals("JE266628", pData.getCaseNumber());
     }
 
     /**
-     * Checks if the isArrestMade method gets the correct field from the String.
+     * Checks if the isArrestMade method gets the correct field from the ArrayList.
+     * @throws ParseException checks if the format is correct when string is parsed.
      */
     @Test
 
@@ -35,7 +40,8 @@ public class PoliceDataTest {
     }
 
     /**
-     * Checks if the getxCord method gets the correct field from the String.
+     * Checks if the getxCord method gets the correct field from the ArrayList.
+     * @throws ParseException checks if the format is correct when string is parsed.
      */
     @Test
 
@@ -47,7 +53,8 @@ public class PoliceDataTest {
     }
 
     /**
-     * Checks if the getyCord method gets the correct field from the String.
+     * Checks if the getyCord method gets the correct field from the ArrayList.
+     * @throws ParseException checks if the format is correct when string is parsed.
      */
 
     @Test
@@ -58,6 +65,49 @@ public class PoliceDataTest {
         PoliceData pData = new PoliceData("1", data);
         assertEquals(1851786, pData.getYCoord());
     }
+
+    /**
+     * Checks if the getDomestic method gets the correct field from the ArrayList.
+     * @throws ParseException checks if the format is correct when string is parsed.
+     */
+
+    @Test
+
+    public void getDomesticCheck() throws ParseException {
+        String[] strSplit = "JE266628,06/15/2021 09:30:00 AM,080XX S DREXEL AVE,0820,THEFT,$500 AND UNDER,STREET,N,N,631,8,06,1183633,1851786,41.748486365,-87.602675062,(41.748486365, -87.602675062)".split(",");
+        ArrayList<String> data = new ArrayList<>(Arrays.asList(strSplit));
+        PoliceData pData = new PoliceData("1", data);
+        assertEquals("NO", pData.getDomestic());
+    }
+
+    /**
+     * Checks if the getWard method gets the correct field from the ArrayList.
+     * @throws ParseException checks if the format is correct when string is parsed.
+     */
+
+    @Test
+
+    public void getWardCheck() throws ParseException {
+        String[] strSplit = "JE266628,06/15/2021 09:30:00 AM,080XX S DREXEL AVE,0820,THEFT,$500 AND UNDER,STREET,N,N,631,8,06,1183633,1851786,41.748486365,-87.602675062,(41.748486365, -87.602675062)".split(",");
+        ArrayList<String> data = new ArrayList<>(Arrays.asList(strSplit));
+        PoliceData pData = new PoliceData("1", data);
+        assertEquals("8", pData.getWard());
+    }
+
+    /**
+     * Checks if the getBeat method gets the correct field from the ArrayList.
+     * @throws ParseException checks if the format is correct when string is parsed.
+     */
+
+    @Test
+
+    public void getBeatCheck() throws ParseException {
+        String[] strSplit = "JE266628,06/15/2021 09:30:00 AM,080XX S DREXEL AVE,0820,THEFT,$500 AND UNDER,STREET,N,N,631,8,06,1183633,1851786,41.748486365,-87.602675062,(41.748486365, -87.602675062)".split(",");
+        ArrayList<String> data = new ArrayList<>(Arrays.asList(strSplit));
+        PoliceData pData = new PoliceData("1", data);
+        assertEquals("631", pData.getBeat());
+    }
+
 
 
 
