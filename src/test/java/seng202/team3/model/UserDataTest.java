b543017f4841a73c749af503006292d2fdb70e28
mga114 <mga114@uclive.ac.nz>
2021-09-27 16:48:17 +1300

    Fixed test cases

--- src/test/java/seng202/team3/model/UserDataTest.java
+++ src/test/java/seng202/team3/model/UserDataTest.java
@@ -18,7 +18,7 @@ public class UserDataTest {
     public void checkDate() throws ParseException {
         String date = "11/26/2020 07:50:00 AM";
         ArrayList<String> uData = new ArrayList<>();
-        uData.add("THEFT"); uData.add("49 MAYS ROAD"); uData.add("41.812610526"); uData.add("-87.723765071"); uData.add("11/26/2020 07:50:00 AM");
+        uData.add("1");uData.add("11/26/2020 07:50:00 AM"); uData.add("49 MAYS ROAD"); uData.add("THEFT");  uData.add("41.812610526"); uData.add("-87.723765071");
         UserData data = new UserData("1", uData);
         assertEquals(date, data.getDate());
     }
