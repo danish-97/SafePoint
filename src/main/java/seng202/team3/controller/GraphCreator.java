6611333c373beac6a3cb27bb6eb84445c246c242
Danish Jahangir <dkj23@uclive.ac.nz>
2021-10-08 11:55:59 +1300

    Committing GraphCreator so that I don't lose it again.

--- src/main/java/seng202/team3/controller/GraphCreator.java
+++ src/main/java/seng202/team3/controller/GraphCreator.java
@@ -2,32 +2,40 @@ package seng202.team3.controller;
 
 import seng202.team3.model.CrimeData;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.WeekFields;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+
 
 public class GraphCreator {
 
-    public Map<String, Integer> formattedDatesIntoGroups(ArrayList<CrimeData> crimeData) {
-
-        List<LocalDate> dates = new ArrayList<>();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss a");
-        for (int i = 0; i < crimeData.size(); i++) {
-            dates.add((LocalDate) formatter.parse(crimeData.get(i).getDate()));
-        }
+    public Map<Integer, List<CrimeData>> formattedDatesIntoGroups(ArrayList<CrimeData> crimeData) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss a");
         WeekFields weekFields = WeekFields.of(DayOfWeek.MONDAY, 7);
-        Map<String, Integer> datesGroupedToWeeks = dates.stream()
-                .collect(Collectors.groupingBy(dates -> dates.get(weekFields.weekOfWeekBasedYear())));
 
-        return datesGroupedToWeeks;
+        Map<Integer, List<CrimeData>> weeklyCrimes = new HashMap<>();
+        for (CrimeData crime: crimeData) {
+            ArrayList<CrimeData> crimesTemp = null;
+            Integer weekOfYear = simpleDateFormat.parse(crime.getDate()).toInstant().atZone(ZoneId.systemDefault()).toLocalDate().get(weekFields.weekOfWeekBasedYear());
+            if (weeklyCrimes.get(weekOfYear) == null) {
+                crimesTemp = new ArrayList<>();
+            } else {
+                crimesTemp = (ArrayList<CrimeData>) weeklyCrimes.get(weekOfYear);
+            }
+            crimesTemp.add(crime);
+            weeklyCrimes.put(weekOfYear, crimesTemp);
+        }
+
+        return weeklyCrimes;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         GraphCreator creator = new GraphCreator();
         ReadCSV readCSV = new ReadCSV();
         ArrayList<CrimeData> crimeData = readCSV.readDataLineByLine("data.csv");
