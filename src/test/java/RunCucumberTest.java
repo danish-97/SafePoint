25f1ac245df488600828aa3c8bf143d6c9997530
Priscilla <pis19@uclive.ac.nz>
2021-10-11 02:34:05 +1300

    Merging fixed POM file, fixing issue in data.csv index error (JE266356)

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -1,10 +1,10 @@
-
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "html:target/cucumber.html"}, snippets = CucumberOptions.SnippetType.CAMELCASE)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber.html"}
+        , snippets = CucumberOptions.SnippetType.CAMELCASE)
 
 public class RunCucumberTest {
 
